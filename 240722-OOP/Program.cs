fd6007cff519dee42a429751b1bdff659301a960
Glaz8212 <lws7129@naver.com>
2024-07-22 15:51:26 +0900

    feat: 실습 과제1

---
+++ 240722-OOP/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _240722_OOP
+{
+    internal class Program
+    {
+        // 과제1 : Character 클래스 선언하기
+        class Character
+        {
+            //필드
+            int level;                //  레벨
+            int hp;                   //  체력
+            float speed;              //  이동 속도
+            float damage;             //  공격력
+
+            void MoveFrontward()      //  전진
+            {
+            }
+            void MoveBackward()       //  후진
+            {
+            }
+            void TurnLeft()           //  좌회전
+            {
+            }
+            void TurnRight()          //  우회전
+            {
+            }
+            void Attack()             //  공격
+            {
+            }
+            void Damaged()            //  피격
+            {
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
