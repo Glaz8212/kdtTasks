babbc5f127343affdedcb6dbce5656eb26e20f00
Glaz8212 <lws7129@naver.com>
2024-08-01 01:20:48 +0900

    Feat: 240801
    과제

---
+++ 240726/240726-Linked&LinkedList/Program.cs
@@ -0,0 +1,212 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _240726
+{
+    internal class Program
+    {
+        #region 과제1
+        //과제1
+        //
+        //프로그램 시작 시 인벤토리는 빈 상태에서 시작한다.
+        //프로그램이 구동되는 동안 입력마다 콘솔에 지속적으로 인벤토리의 상태를 표시한다
+        //인벤토리는 최대 9개의 아이템을 가질 수 있다.
+        //인벤토리는 빈칸 없이 앞부터 채워서 가진다
+        //숫자키 0을 누르면 랜덤으로 아이템의 종류를 획득하고 인벤토리에 추가한다
+        //숫자키 1~9를 누르면 해당하는 숫자의 아이템을 제거한다
+        //구현 클래스
+        //Inventory
+        //Item
+        //Potion : Item
+        //Weapon : Item
+        //Armor : Item
+        //Accessory : Item
+        //Food : Item
+
+        #region 아이템 클래스
+        public class Item
+        {
+            public string _name { get; set; }
+
+            public Item(string name)
+            {
+                _name = name;
+            }
+        }
+
+        public class Potion : Item
+        {
+            public Potion() : base("포션") { }
+        }
+
+        public class Weapon : Item
+        {
+            public Weapon() : base("무기") { }
+        }
+        public class Armor : Item
+        {
+            public Armor() : base("방어구") { }
+        }
+        public class Accessory : Item
+        {
+            public Accessory() : base("장신구") { }
+        }
+        public class Food : Item
+        {
+            public Food() : base("음식") { }
+        }
+        #endregion
+
+        #region 1에서 5까지 랜덤 추출
+        static int Rand1to5()
+        {
+            Random random = new Random();
+            return random.Next(1, 6);
+        }
+        #endregion
+
+        #region 아이템 얻기
+        static Item GetRandomItem()
+        {
+            switch (Rand1to5())
+            {
+                case 1:
+                    return new Potion();
+                case 2:
+                    return new Weapon();
+                case 3:
+                    return new Accessory();
+                case 4:
+                    return new Food();
+                case 5:
+                    return new Armor();
+                default:
+                    return null;
+            }
+        }
+        #endregion
+
+        #region 인벤토리
+        public class Inventory
+        {
+            public List<Item> inventory = new List<Item>(9);
+
+            public void Add(Item item)
+            {
+                if (inventory.Count < 9)
+                {
+                    inventory.Add(item);
+                    Console.WriteLine($"{item._name}이/가 인벤토리에 추가되었습니다.");
+                }
+                else
+                {
+                    Console.WriteLine("인벤토리가 가득 찼습니다.");
+                }
+            }
+
+            public void Remove(int index)
+            {
+                Item item = inventory[index - 1];
+                inventory.RemoveAt(index - 1);
+                Console.WriteLine($"{item._name}이/가 인벤토리에서 제거되었습니다.");
+            }
+
+            public void Write()
+            {
+                Console.Write("인벤토리 : ");
+                foreach (Item item in inventory)
+                {
+                    Console.Write($"{item._name} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        #endregion
+
+        #region 본문
+        static void Main(string[] args)
+        {
+            Inventory inventory = new Inventory();
+
+            while (true)
+            {
+                Console.WriteLine("0을 누르시면 아이템이 랜덤으로 추가되며, 1~9를 누르시면 해당 아이템이 제거됩니다.");
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                int value;
+                int.TryParse(input.KeyChar.ToString(), out value);
+
+                if (value == 0)
+                {
+                    inventory.Add(GetRandomItem());
+                    inventory.Write();
+                }
+                else if (1 <= value && value <= 9)
+                {
+                    int index = value;
+                    inventory.Remove(index);
+                    inventory.Write();
+                }
+                else
+                {
+                    Console.WriteLine("잘 못 입력하셨습니다");
+                }
+            }
+        }
+        #endregion
+        #endregion
+
+
+        #region 과제2
+        //과제2
+        //
+        //1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다.
+        //이제 순서대로 K번째 사람을 제거한다.
+        //한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다.
+        //이 과정은 N명의 사람이 모두 제거될 때까지 계속된다.
+        //원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다.
+        //예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+        //
+        //아래의 요세푸스 순열에 대한 설명을 보고 N와 K 가 주어지면 결과를 출력하는 프로그램으로 구현하시오.
+
+
+        static string Josephus(int x, int y)
+        {
+            LinkedList<int> josephus = new LinkedList<int>();
+            List<int> josephusList = new List<int>();
+            
+            for (int i = 1; i <= x; i++)
+            {
+                josephus.AddLast(i);
+            }
+
+            LinkedListNode<int> here = josephus.First;
+
+            while (josephus.Count > 0)
+            {
+                for (int i = 1; i < y; i++)
+                {
+                    if (here.Next != null)
+                    {
+                        here = here.Next;
+                    }
+                    else
+                    {
+                        here = josephus.First;
+                    }
+                }
+
+                josephusList.Add(here.Value);
+
+                josephus.Remove(here);
+
+            }
+
+            string returnJosephus;
+            
+            foreach (int i in josephusList)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
+#endregion
---
+++ 240726/240726-Stack&Queue/Program.cs
@@ -0,0 +1,182 @@
+﻿namespace _240726_Stack_Queue
+{
+    internal class Program
+    {
+        #region 과제1
+        //과제1
+        //
+        //다음의 조건을 충족하는 괄호 검사기 프로그램을 구현하시오
+        //괄호가 바르게 짝지어졌다는 것은 열렸으면 짝지어서 닫혀야 한다는 뜻이다
+        //텍스트를 입력으로 받아서 괄호가 유효한지 판단하는 프로그램을 작성하자
+        //검사할 괄호 : [], {}, ()
+        //예시 : () 완성, (() 미완성, [[(){}]] 완성, [(})] 미완성
+
+        static void CheckCorrectBracket(string input)
+        {
+            Stack<char> stack1 = new();
+            Stack<char> stack2 = new();
+            Stack<char> stack3 = new();
+            Stack<char> stack4 = new();
+            Stack<char> stack5 = new();
+            Stack<char> stack6 = new();
+
+            char[] inputArray = input.ToCharArray();
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                char inputChar = inputArray[i];
+
+                switch (inputChar)
+                {
+                    case '(':
+                        stack1.Push(inputChar);
+                        break;
+                    case ')':
+                        stack2.Push(inputChar);
+                        break;
+                    case '{':
+                        stack3.Push(inputChar);
+                        break;
+                    case '}':
+                        stack4.Push(inputChar);
+                        break;
+                    case '[':
+                        stack5.Push(inputChar);
+                        break;
+                    case ']':
+                        stack6.Push(inputChar);
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+            }
+            if (stack1.Count == stack2.Count)
+            {
+                Console.WriteLine("소괄호가 완성되었습니다.");
+            }
+            else
+            {
+                Console.WriteLine("소괄호가 미완성입니다.");
+            }
+
+            if (stack3.Count == stack4.Count)
+            {
+                Console.WriteLine("중괄호가 완성되었습니다.");
+            }
+            else
+            {
+                Console.WriteLine("중괄호가 미완성입니다.");
+            }
+
+            if (stack5.Count == stack6.Count)
+            {
+                Console.WriteLine("대괄호가 완성되었습니다.");
+            }
+            else
+            {
+                Console.WriteLine("대괄호가 미완성입니다.");
+            }
+        }
+        #endregion
+
+        #region 과제2
+
+        //과제2
+        //
+        //다음의 조건을 충족하는 작업 일정 계산기 프로그램을 구현하시오
+        //하루에 8시간씩 일하는 회사가 있으며 남는시간 없이 주어진 일을 계속한다
+        //각 작업이 몇시간이 걸리는 작업인지 포함하는 배열을 받을 때,
+        //각각의 작업이 끝나는 날짜를 결과 배열로 출력하는 함수를 작성하자
+        //예시
+        //입력 : [4, 4, 12, 10, 2, 10]
+        //출력 : [1, 1, 3, 4, 4, 6]
+        //해석
+        //1일차 : 0번째 작업의 4/4 완료 + 1번쨰 작업의 4/4 완료.
+        //2일차 : 2번째 작업의 8/12 완료
+        //3일차 : 2번째 작업의 4/12 완료 + 3번째 작업의 4/10 완료
+        //4일차 : 3번째 작업의 6/10 완료 + 4번째 작업의 2/2 완료
+        //5일차 : 5번째 작업의 8/10 완료
+        //6일차 : 5번째 작업의 2/10 완료
+
+        static void WorkCalculater()
+        {
+            //일단, 작업 시간들을 저장할 queue를 생성하고 데이터를 입력받는다.
+            Queue<int> que = new();
+
+            while (true)
+            {
+                Console.Write("작업 시간을 입력하세요. 999를 입력시 입력을 종료합니다.");
+                string input = Console.ReadLine();
+                int workTime;
+                bool success = int.TryParse(input, out workTime);
+
+                if (success)
+                {
+                    if (workTime == 999)
+                    {
+                        Console.WriteLine("입력을 종료합니다.");
+                        break;
+                    }
+
+                    else if (workTime > 0)
+                    {
+                        que.Enqueue(workTime);
+                    }
+                    else
+                    {
+                        Console.WriteLine("양수를 입력해주세요.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("양의 정수를 입력해주세요");
+                }
+            }
+
+
+            //이제 작업 시간이 저장된 queue가 생성되어있는 상태이다.
+            //이제 dequeue를 반복하면서 8로 나눌때 마다 다음 일자로 넘어가는걸 만들면 될 것 같다.
+
+            //int day = 1;
+            //int time = 8;
+
+            //while (que.Count > 0)
+            //{
+            //    int work = que.Dequeue();
+
+            //    while (work > 0)
+            //    {
+            //        if (work <= time)
+            //        {
+            //            Console.WriteLine($"작업이 {day}일에 완료되었습니다.");
+            //            time -= work;
+            //            work = 0;
+            //        }
+            //        else
+            //        {
+            //            work -= time;
+            //            Console.WriteLine($"작업이 {day}일차에 {time}만큼 완료되었습니다.");
+            //            day++;
+            //            time = 8;
+            //        }
+            //    }
+            //}
+
+            // 어렵다
+
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Console.Write("괄호를 검사할 문자열을 입력해주세요 : ");
+            string input = Console.ReadLine();
+            CheckCorrectBracket(input);
+
+            WorkCalculater();
+        }
+    }
+}
