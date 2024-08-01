b0f010ef12dd2c4841f04f5ed908abcd41bf3390
Glaz8212 <lws7129@naver.com>
2024-08-01 21:53:22 +0900

    feat: 240801과제

---
+++ 240801/240801-Dictionary/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Numerics;
+
+namespace _240801_Dictionary
+{
+    internal class Program
+    {
+        #region 과제 1
+        //과제 1 몬스터 데이터베이스 구현하기
+
+        //몬스터 클래스 생성, 인스턴스 생성 시 생성자를 사용해 데이터 초기화
+        public class Monster
+        {
+            public string name;
+            public int hp;
+
+            public Monster(string name, int hp)
+            {
+                this.name = name;
+                this.hp = hp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //몬스터 이름의 string Key 값으로 딕셔너리에 저장, 5종류 이상
+            Dictionary<string, Monster> monsterData = new Dictionary<string, Monster>();
+
+            monsterData.Add("모르고트", new Monster("모르고트", 10399));
+            monsterData.Add("라단", new Monster("라단", 9572));
+            monsterData.Add("고드릭", new Monster("고드릭", 6080));
+            monsterData.Add("모그", new Monster("모그", 14000));
+            monsterData.Add("말레니아", new Monster("말레니아", 18473));
+
+            #endregion
+
+            #region 심화 과제
+            //다음의 조건을 충족하는 MyDictionary<TKey, TValue> 클래스를 구현하시오
+            //Dictionary<TKey, TValue> 클래스의 사용을 금지하며, 배열을 사용해 구현한다.
+            //클래스의 인스턴스 생성 시 최초 193의 크기를 가진다.
+            //충돌이 발생하는 경우 선형탐사를 통해 충돌상황을 해결한다.
+            //공간 사용률이 70 % 를 넘기는 경우 재해싱 과정을 진행한다.
+            //아래 필수 구현 메서드 외 내부 동작을 위한 필드 및 메서드 추가는 허용한다.
+            //구현 메서드
+            //Add(TKey key, TValue value) : 지정한 키를 기준으로 값을 추가한다.
+            //Remove(TKey key) : 지정한 키를 기준으로 값을 찾아 삭제한다.
+            //Contains(TKey key) : 지정한 키를 기준으로 값이 포함되어 있는지 확인한다.
+        }
+        public class MyDictionary
+        {
+            string[] myDictionary = new string[193];
+
+            public MyDictionary()
+            {
+                string[] myDictionary = new string[193];
+            }
+
+            public void Add(int key, string value)
+            {
+                if (myDictionary[key] == null)
+                {
+                    for (key = 0; key < myDictionary.Length; key++)
+                    {
+                        myDictionary[key] = value;
+                    }
+                }
+                else
+                {
+                    while (myDictionary[key] != null)
+                    {
+                        key = key++;
+                    }
+                }
+            }
+
+            if 
+        }
+    }
+}
+    #endregion
---
+++ 240801/240801-Graph/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace _240801_Graph
+{
+    internal class Program
+    {
+        public abstract class Graph  //그래프로 나타낼 메서드 구현
+        {
+            public abstract void Connect(int from, int to);
+            public abstract void DisConnect(int from, int to);
+            public abstract bool IsConnect(int from, int to);
+        }
+        public class ListGraph : Graph //리스트그래프 구현과, 사용할 메서드 구현
+        {
+            private List<int>[] graph;
+
+            public ListGraph(int vertex)
+            {
+                graph = new List<int>[vertex];
+                for (int i = 0; i < vertex; i++)
+                {
+                    graph[i] = new List<int>();
+                }
+            }
+
+            public override void Connect(int from, int to)
+            {
+                graph[from].Add(to);
+            }
+
+            public override void DisConnect(int from, int to)
+            {
+                graph[from].Remove(to);
+            }
+
+            public override bool IsConnect(int from, int to)
+            {
+                return graph[from].Contains(to);
+            }
+        }
+        public class MatrixGraph : Graph // 행렬 그래프 구현과, 사용할 메서드 구현
+        {
+            private bool[,] graph;
+
+            public MatrixGraph(int vertex)
+            {
+                graph = new bool[vertex, vertex];
+            }
+
+            public override void Connect(int from, int to)
+            {
+                graph[from, to] = true;
+            }
+
+            public override void DisConnect(int from, int to)
+            {
+                graph[from, to] = false;
+            }
+
+            public override bool IsConnect(int from, int to)
+            {
+                return graph[from, to];
+            }
+        }
+        static void Main(string[] args)
+        {
+            // 사진 그래프 내용 리스트그래프로 만들기
+            ListGraph listGraph = new ListGraph(8);
+
+            listGraph.Connect(0, 3);
+            listGraph.Connect(0, 4);
+
+            listGraph.Connect(2, 4);
+            listGraph.Connect(2, 6);
+
+            listGraph.Connect(3, 7);
+
+            listGraph.Connect(4, 6);
+            listGraph.Connect(4, 7);
+
+            listGraph.Connect(5, 7);
+
+            listGraph.Connect(6, 2);
+            listGraph.Connect(6, 5);
+            listGraph.Connect(6, 7);
+
+            listGraph.Connect(7, 6);
+            
+
+            //출력
+            for (int i = 0; i < 8 ; i++)
+            {
+                Console.WriteLine($"{i}노드 : ");
+                for (int j = 0; j < 8; j++)
+                {
+                    if (listGraph.IsConnect(i, j))
+                    {
+                        Console.WriteLine($"   {j}노드");
+                    }
+                }
+            }
+
+            // 사진 그래프 내용 행렬그래프로 만들기
+            MatrixGraph matrixGraph = new MatrixGraph(8);
+            matrixGraph.Connect(0, 3);
+            matrixGraph.Connect(0, 4);
+
+            matrixGraph.Connect(2, 4);
+            matrixGraph.Connect(2, 6);
+
+            matrixGraph.Connect(3, 7);
+
+            matrixGraph.Connect(4, 6);
+            matrixGraph.Connect(4, 7);
+
+            matrixGraph.Connect(5, 7);
+
+            matrixGraph.Connect(6, 2);
+            matrixGraph.Connect(6, 5);
+            matrixGraph.Connect(6, 7);
+
+            matrixGraph.Connect(7, 6);
+
+            for (int i = 0;i < 8 ; i++)
+            {
+                Console.WriteLine($"{i}노드 : ");
+                for (int j = 0;j < 8; j++)
+                {
+                    if (matrixGraph.IsConnect(i, j))
+                    {
+                        Console.WriteLine($"   {j}노드");
+                    }
+                }
+            }
+        }
+    }
+}
